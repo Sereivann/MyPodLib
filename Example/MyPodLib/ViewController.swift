de562844f60936bd58d9fde9048143e122f98e4b
Sereivann YONG <sereivann.yong@gmail.com>
2016-07-28 15:31:44 +0200

    Initial commit

---
+++ Example/MyPodLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyPodLib
+//
+//  Created by Sereivann YONG on 07/28/2016.
+//  Copyright (c) 2016 Sereivann YONG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
